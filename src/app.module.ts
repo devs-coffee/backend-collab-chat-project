a5cd432b9aed85976bbbbc59e4d66cba8fe27d2f
julienb86 <julien.bouafia@orange.fr>
2023-06-11 13:30:37 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AutoMapping } from './core/automapping';
 import { ServersModule } from './servers/servers.module';
-import { EventsModule } from './events/events.module';
 import { MessagesModule } from './messages/messages.module';
 import { ChannelModule } from './channels/channel.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, MessagesModule, ChannelModule, AuthenticationModule, ServersModule, EventsModule, AutomapperModule.forRoot({
+  imports: [PrismaModule, UsersModule, MessagesModule, ChannelModule, AuthenticationModule, ServersModule, AutomapperModule.forRoot({
     strategyInitializer: classes(),
 }),],
   controllers: [AppController],
