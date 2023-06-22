8b2e158c559e16254aa22b87e6497bf08d288405
julienb86 <julien.bouafia@orange.fr>
2023-06-22 19:02:37 +0200

    set up user tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,23 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Mapper } from '../core/mapper';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { createMapper } from '@automapper/core';
+import { getMapperToken } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
+import { OperationResult } from '../core/OperationResult';
+import { UserDto } from '../dtos/users/user.dto';
+import { errorConstant } from '../constants/errors.constants';
+import { BadRequestException } from '@nestjs/common';
+import { PrismaPromise, User } from '@prisma/client';
+import { randomUUID } from 'crypto';
 
 describe('UsersController', () => {
   let controller: UsersController;
-
+  let usersService: UsersService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService, Mapper],
+      providers: [UsersService, { provide: getMapperToken(), useValue: createMapper({
+        strategyInitializer: classes(),
+      }), 
+    }],
       imports: [PrismaModule]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
 });
+
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersService } from './users.service';
+import { getMapperToken } from '@automapper/nestjs';
+import { createMapper } from '@automapper/core';
+import { classes } from '@automapper/classes';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, { provide: getMapperToken(), useValue: createMapper({
+        strategyInitializer: classes(),
+      }), 
+    }],
       imports: [PrismaModule],
     }).compile();
 
