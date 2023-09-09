06289631417938b8dafa704ab2951450fad4694f
julienb86 <julien.bouafia@orange.fr>
2023-09-09 11:25:56 +0200

    modify updateUserDto

--- src/dtos/users/update-user.dto.ts
+++ src/dtos/users/update-user.dto.ts
@@ -1,31 +1,7 @@
-import { AutoMap } from "@automapper/classes";
 import { PartialType } from "@nestjs/swagger";
-import { IsOptional, Matches  } from 'class-validator';
-import { Match } from "../../core/match.decorator";
 import { CreateUserDto } from "./create-user.dto";
-import { UserDto } from "./user.dto";
-
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
     oldPassword?: string;
     refreshToken?: string;
 }
-
-// export class UpdateUserDto implements IUser {
-//     oldPassword?: string;
-
-//     @AutoMap()
-//     pseudo?: string;
-
-//     @AutoMap()
-//     picture?: string;
-
-//     @IsOptional()
-//     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'Mot de passe trop faible'})
-//     @AutoMap()
-//     password?: string;
-
-//     @IsOptional()
-//     @Match('password')
-//     passwordConfirm?: string;
-// }
